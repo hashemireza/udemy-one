6129c947af2cabbfc50b5b7ef3d60542a8437b62
Reza Hashemi <hahsemi.reza1993@gmail.com>
2020-06-15 19:15:47 +0430

    Generate Question model via artisan model generator

---
+++ app/Question.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+	protected $fillable = ['title', 'body'];
+
+    public function user()
+    {
+    	return $this->belongsTo(User::class);
+
+    	// $question = Question::find(1);
+    	// $question->user->name;
+    }
+
+
+    public function setTitleAttribute($value)
+    {
+    	$this->attributes['title'] = $value;
+    	$this->attributes['slug'] = str_slug($value);
+    }
+}
