0c95752567695bd053ddcde1bae367308491cc16
Reza Hashemi <hahsemi.reza1993@gmail.com>
2020-07-11 13:54:48 +0430

    Display Answer For Question

--- app/Answer.php
+++ app/Answer.php
@@ -15,4 +15,26 @@ class Answer extends Model
     {
     	return $this->belongsTo(User::class);
     }
+
+    public function getBodyHtmlAttribute()
+    {
+        return \Parsedown::instance()->text($this->body);
+    }
+
+    public static function boot()
+    {
+    	parent::boot();
+
+    	static::created(function ($answer){
+    		$answer->question->increment('answers_count');
+    	});
+    }
+
+    public function getCreatedDateAttribute()
+    {
+    	return $this->created_at->diffForHumans();
+    }
+
+    
+
 }
